ca4c4c4a89e11e279746efd47601a765ef8e3451
Beatriz <biasilva.lima118@gmail.com>
2020-06-16 10:51:30 -0300

    Classe jogador criada. Agora colocando os atributos e metodos

---
+++ Jogador.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Aula18Dojo1
+{
+    public class Jogador
+    {
+        public string Nome { get; set; }
+         public string Posicao { get; set; }
+          public DateTime DatadeNascimento { get; set; }
+          public string Nacionalidade { get; set; }
+           public float Altura { get; set; }
+           public float peso { get; set; }
+
+    }
+}
